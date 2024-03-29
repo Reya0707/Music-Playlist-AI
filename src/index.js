a9ac62870fc1cb6c9762ea9d858ecc8feb70e9b9
Reya <150582472+Reya0707@users.noreply.github.com>
2024-03-29 09:38:35 +0000

    Changed my styling and created a new prompt for my AI generator.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayWaitingMessage() {
-  document.getElementById("playlist-response").innerHTML = "Generating playlist...";
+  document.getElementById("playlist-response").innerHTML = "Generating playlist...🎶🎶";
 }
 
 function displayplaylist(response) {
@@ -21,7 +21,7 @@ function generateplaylist(event) {
   let instructionsInput = document.querySelector("#user-instructions"); 
   let apiKey = "36c26f017e4ae34tb6a9679bb25odafe";
   let context =
-    "You are an AI Music expert that loves to curate playlists. The playlist must be provided in HTML format with a suggestion of 10 songs. The songs should each include the name of the artist. Sign the playlist at the end with Reya AI and make sure only Reya AI is bold. Provide a title for each playlist and font size should be 15px and bold. Make sure the follow the user instructions.";
+    "You are an AI Music expert that loves to curate playlists. The playlist must be provided in HTML format with a suggestion of 10 songs only. The songs should each include the name of the artist but don't add any bullet points or numbering. The Font Size for the songs should be 14px. Sign the playlist at the end with ✨MusicDo AI✨ and make sure only MusicDo AI is bold and 17px. Provide a relevant title for each playlist, underline it and font size should be 17px and bold. Make sure the follow the user instructions value. The playlist will generate songs of artists similar to the input provided. For example, if I type in the band Korn, it should suggest songs from similar bands such as Limp Bizkit, SOAD, Linkin Park. If I type in Adele, it will suggest artist like Beyonce, Taylor Swift, Christina, Britney. If I type in Tool it will suggest bands like A Perfect Circle, Pineapple Thief and so on....Do this for all type of artists entered. Only provide maximum one song of the artist entered in the value. The other songs should be from different artists similar to the value input.";
   let prompt = `User instructions: Generate a playlist about ${instructionsInput.value}`;
   
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
@@ -35,3 +35,4 @@ function generateplaylist(event) {
 
 let playlistFormElement = document.querySelector("#playlist-generator-form");
 playlistFormElement.addEventListener("submit", generateplaylist);
+
